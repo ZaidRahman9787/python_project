93b815bcc756b1c6f7a79567ef2f8927a7d7e8d5
Arshad Ali <comeback.hoga.fr@gmail.com>
2026-01-03 18:56:50 +0530

    First Commit

---
+++ day01.py
@@ -0,0 +1,68 @@
+import time 
+
+def print_with_delay(text, delay=0.1):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()  
+name = input("Enter your name:-")
+if name == "":
+    print("Error: Nameh cannot be empty.")
+    exit()
+
+try:
+    age = int(input("enter your age:-"))
+except ValueError:
+    print ("Erorr,enter a number")
+    exit()
+
+carrier_stage=""
+if age <= 18:
+    carrier_stage += "student"
+elif age > 18 and age<25:
+    carrier_stage += "Early Professional"
+else:
+    carrier_stage += "professional"
+city = input("Enter the city you live in:-").strip().capitalize()
+
+primary_skil = input("what are your primary skills:-")
+
+print_with_delay("tell us how good you are at your skill",0.01)
+try:
+    skill_level = input("choose between begginer, intermediate or expert: ").capitalize()
+    
+    if skill_level not in ("begginer", "intermediate", "expert"):
+        raise ValueError 
+except ValueError:
+    print("Error: Invalid option.")
+    exit()
+readiness_tag= ""
+if skill_level=="begginer":
+    readiness_tag += "foundation"
+elif skill_level=="intermediate":
+    readiness_tag += "Junior intern"
+else:
+    readiness_tag += "production ready"
+learning_recommendation=""
+if skill_level=="begginer":
+    learning_recommendation += "Focus on core fundamentals and consistency"
+elif skill_level=="intermediate":
+    learning_recommendation += "Focus on core fundamentals and consistency"
+else:
+    learning_recommendation += "Contribute to production-grade systems"
+print("=" * 45)
+print("        CANDIDATE PROFILE CARD")
+print("=" * 45)
+
+print("Name          :",name)
+print("Age           :",age)
+print("City          :",city)
+print("Primary Skill :",primary_skil)
+print("Skill Level   :",skill_level)
+
+print()
+print("Career Stage  :",carrier_stage)
+print("Readiness Tag :",readiness_tag)
+print("Recommendation:",learning_recommendation)
+print("=" * 45)
+
